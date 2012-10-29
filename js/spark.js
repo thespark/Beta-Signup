b0e79bf569025de6586bf7051c7ea81aa36a5f20
Mark <mark@thespark.org>
2012-10-29 02:43:48 -0700

    Added selector caching

--- js/spark.js
+++ js/spark.js
@@ -1,24 +1,38 @@
 $(document).ready(function() {
-	
+
+/* Selector caching ************************************************************/
+var content = $('#content');
+var home = $('#content-home');
+var jobs = $('#content-jobs');
+var signup = $('#signup');
+var back = $('.back');
+/*******************************************************************************/
+
+/* Scroll to position ******************************************************/
+function scroll_to(position) {
+	$('body').animate({scrollTop: position}, 500);
+}
+/****************************************************************************/
+ 
 /* Sign-up Overlay *************************************************************/
 	// shows overlay when "sign up" links are clicked
 	$('a.beta').click(function(event){
 		event.preventDefault();
-		$("#signup").show();
+		signup.show();
 	});
 	// hides overlay on clicking corner  X
 	$('.closebox').click(function() {
-		$("#signup").hide();
+		signup.hide();
 	});
 	// hides overlay on submit
 	$('#mc-embedded-subscribe-form').submit(function(){
-		$('#signup').hide();
+		signup.hide();
 	});
 	// hides overlay on esc
     $(document).keyup(function(e) {
         // user hits esc when the dialog is open
         if (e.keyCode == 27 && $('#signup:visible')) {
-            $('#signup').hide();
+            signup.hide();
         }
 	});
 		
@@ -28,18 +42,18 @@ $(document).ready(function() {
 	// Intercept privacy and terms links
 	$('a.privacy, a.terms').click(function(event) {
 		event.preventDefault();
-		// Prevents flicker on slower machines
-		$('#content').hide();
-		if($('#content-jobs').is(':visible')) {
-			$('#content-jobs').hide();
+		// Mitigates flicker on slower machines
+		content.hide();
+		if(jobs.is(':visible')) {
+			jobs.hide();
 		}
 		link = this.href.match(/\/(\w*\.html)/)[1];
 		$.get(link, function(data) {
-			$('#content').empty();
-			$('#content').html($(data).filter('#content').html());
-			$('#content-home').fadeOut(200);
-			$('.back').fadeIn(500);
-			$('#content').fadeIn(500); 
+			content.empty();
+			content.html($(data).filter('#content').html());
+			home.fadeOut(200);
+			back.fadeIn(500);
+			content.fadeIn(500); 
 			scroll_to(0);
 		});
 
@@ -49,34 +63,28 @@ $(document).ready(function() {
 	$('a.back').click(function(event) {
 		event.preventDefault();
 		// Workaround for code execution prevention in loaded pages
-		if($('#content-jobs').is(':visible')) {
-			$('#content-jobs').hide();
-			$('#content-home').fadeIn(500);
-			$('.back').hide();
+		if(jobs.is(':visible')) {
+			jobs.hide();
+			home.fadeIn(500);
+			back.hide();
 		} else {
-			$('#content').hide();
-			$('.back').hide(); 
-			$('#content-home').fadeIn(500);
+			content.hide();
+			back.hide(); 
+			home.fadeIn(500);
 			scroll_to(0)
 		}
 	});
 	// Intercept jobs links
 	$('a.job').click(function(event) {
-		$('#content-home').hide();
+		home.hide();
 		event.preventDefault();
-		$('#content').hide();
-		$('#content-jobs').fadeIn(500);
+		content.hide();
+		jobs.fadeIn(500);
 		scroll_to($(this.hash).offset().top)
-		$('.back').fadeIn(500); 
+		back.fadeIn(500); 
 	});
 /****************************************************************************/
 
-
-/* Scroll to position ******************************************************/
-function scroll_to(position) {
-	$('body').animate({scrollTop: position}, 500);
-}
-/****************************************************************************/
 
 });    
 
