f7ef20d8928e85e76719e09352265269c0c985d3
Mark <mark@thespark.org>
2012-10-29 02:05:01 -0700

    Complete javascript recode to facilitate Keith's requirements:
    - jQuery will not execute code on imported (.load()/.get()/.ajax()) content. Had to hide #content-jobs and #content-home in page to allow for scrolling on the jobs div inside of the jobs page.
    - Added compliance for non-Javascript fallback (including signup form)
    - Removed extraneous jQuery plugins
    
    TODO:
    
    - Add transitions

--- js/spark.js
+++ js/spark.js
@@ -1,138 +1,81 @@
 $(document).ready(function() {
-    
-    function toggle_jobs_display(callback) {
-        var def1 = $('#content-jobs').fadeToggle('hidden');            
-        var def2 = $('#content-home').fadeToggle('hidden');
-        var def3 = $('#header a.back').toggleClass('hidden');
-
-        // fire off the callback
-        if (typeof callback === 'function') {
-            $.when(def1, def2, def3).then(callback);
-        }
-    }
-    
-    function scrollto_job(hash) {
-        var scrollTo = $(hash),
-            container = scrollTo.parent();
-        
-        $('body').animate(
-            {scrollTop: scrollTo.offset().top},
-            500,
-            'swing',
-            function() {
-                location.hash = hash;
-            }
-        );
-    }
-    
-    function load_and_scrollto_job(hash) {
-        $('#content-jobs').load('jobs.html', function() {
-            toggle_jobs_display(function() {
-                scrollto_job(hash);
-            });
-        });
-    }
-    
-
-    /* Signup/Login Overlay */
-	$('.overlay').click(function() {
-		$(this).addClass('hidden');
-	}).children().click(function(event) {
-		/* stop clicks in the box from bubbling up to .overlay */
-        event.stopImmediatePropagation();
-
-/* 		return false; */
+	
+/* Sign-up Overlay *************************************************************/
+	// shows overlay when "sign up" links are clicked
+	$('a.beta').click(function(event){
+		event.preventDefault();
+		$("#signup").show();
 	});
-
-    $('.overlay .signup .closebox').click(function() {
-        $(this).closest('.overlay').addClass('hidden');
-    });
-    $('a.beta.signup').click(function() {
-        $('#signup').removeClass('hidden');
-    });
+	// hides overlay on clicking corner  X
+	$('.closebox').click(function() {
+		$("#signup").hide();
+	});
+	// hides overlay on submit
+	$('#mc-embedded-subscribe-form').submit(function(){
+		$('#signup').hide();
+	});
+	// hides overlay on esc
     $(document).keyup(function(e) {
         // user hits esc when the dialog is open
-        if (e.keyCode == 27 && ! $('#signup').hasClass('hidden')) {
-            $('#signup').addClass('hidden');
+        if (e.keyCode == 27 && $('#signup:visible')) {
+            $('#signup').hide();
         }
-    });
+	});
+		
+/******************************************************************************/		
+
+/* Switch out content ********************************************************/
+	// Intercept privacy and terms links
+	$('a.privacy, a.terms').click(function(event) {
+		event.preventDefault();
+		$('#content-home').hide();
+		$('#content').show();
+		if($('#content-jobs').is(':visible')) {
+			$('#content-jobs').hide();
+		}
+		link = this.href.match(/\/(\w*\.html)/)[1];
+		$.get(link, function(data) {
+			$('#content').empty();
+			$('#content').html($(data).filter('#content').html());
+			$('.back').show(); 
+			scroll_to(0);
+		});
+
+	});
+	
+	// Intercept back to home link
+	$('a.back').click(function(event) {
+		event.preventDefault();
+		// Workaround for code execution prevention in loaded pages
+		if($('#content-jobs').is(':visible')) {
+			$('#content-jobs').hide();
+			$('#content-home').show();
+			$('.back').hide();
+		} else {
+			$('#content').hide();
+			$('.back').hide(); 
+			$('#content-home').show();
+			scroll_to(0)
+		}
+	});
+	// Intercept jobs links
+	$('a.job').click(function(event) {
+		$('#content-home').hide();
+		event.preventDefault();
+		$('#content').hide();
+		$('#content-jobs').show();
+		scroll_to($(this.hash).offset().top)
+		$('.back').show(); 
+	});
+/****************************************************************************/
 
 
-    /* Issue comments and solutions handler */
-/*
-    $('.issue .view.solutions').click(function() {
-        $(this).closest('.issue-issue').children('.issue-comments, .issue-comments-top').addClass('hidden');
-        $(this).closest('.issue-issue').children('.issue-solutions, .issue-solutions-top').toggleClass('hidden');
-        $(this).closest('.issue-details').children('.view.comments').removeClass('selected');
-        $(this).toggleClass('selected');
-    });
-    $('.issue .view.comments').click(function() {
-        $(this).closest('.issue-issue').children('.issue-solutions, .issue-solutions-top').addClass('hidden');
-        $(this).closest('.issue-issue').children('.issue-comments, .issue-comments-top').toggleClass('hidden');
-        $(this).closest('.issue-details').children('.view.solutions').removeClass('selected');
-        $(this).toggleClass('selected');
-    });
-*/
-    
-    /* Tooltip closebutton */
-/*
-    $('.vote-tooltip-close').click(function () {
-        $(this).closest('.vote-tooltip-container').addClass('hidden');
-    });
-*/
+/* Scroll to position ******************************************************/
+function scroll_to(position) {
+	$('body').animate({scrollTop: position}, 500);
+}
+/****************************************************************************/
 
-    
-    // Job stuff
-    $('a.job').click(function(event){
-        var hash = this.hash;
+});    
 
-        // stop normal behavior until we load or switch content
-        event.stopImmediatePropagation();
-        event.preventDefault();
 
-        if ($('#content-jobs').text().length > 0) {
-            // jobs content loaded
-            toggle_jobs_display(function() {
-                // location.hash = hash;
-                scrollto_job(hash);
-                console.log('a.job .click if loaded ' + hash);
-            });            
-        } else {
-            // have to load jobs
-            load_and_scrollto_job(hash);          
-        }
-    });
-    
-    // Deal with direct link clicks
-    $('a.back').click(function() {
-        location.hash = '';
-        toggle_jobs_display();        
-        
-        return false;
-    });
-    
-    // Handle for URLs
-    if (location.hash.length > 0) {
-        hash = location.hash;
-        location.hash = '';
-        
-        switch(hash) {
-            case '#Partnerships-and-Fundraising-Lead':
-                load_and_scrollto_job(hash);
-                break;
-            case '#Offline-Communications-Lead':
-                load_and_scrollto_job(hash);
-                break;
-            case '#Crowd-sourcing-Expert':
-                load_and_scrollto_job(hash);
-                break;
-            case '#Front-end-Developer':
-                load_and_scrollto_job(hash);
-                break;
-            case '#Application-Developer':
-                load_and_scrollto_job(hash);
-                break;
-        }
-    }
-
-});
